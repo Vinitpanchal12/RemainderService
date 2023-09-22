6d571902aba7bebe6d497971a8ac5916ef5e1560
Vinitjp123 <vinitjpanchal2002@gmail.com>
2023-09-22 19:46:06 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express();
+const bodyParser = require('body-parser');
+//const apiRoutes = require('./routes/index');
+//const db = require('./models/index');
+
+const {PORT} = require('./config/serverConfig');
+
+const setupAndStartServer=() =>{
+    
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({extended:true}));
+
+    //app.use('/api',apiRoutes);
+    
+    app.listen(PORT, ()=>{
+        console.log(`server started at ${PORT}`);
+        if(process.env.DB_SYNC){
+            db.sequelize.sync({alter: true});
+        }
+    })
+}
+setupAndStartServer();
