960d7935e0e285dbff877931c00fce94c33d5744
Vinitjp123 <vinitjpanchal2002@gmail.com>
2023-09-22 20:51:26 +0530

    setup smtp using nodemailer

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 const express = require('express')
-const app = express();
+
 const bodyParser = require('body-parser');
 //const apiRoutes = require('./routes/index');
 //const db = require('./models/index');
-
+const {sendBasicEmail} = require('./services/emailService')
 const {PORT} = require('./config/serverConfig');
 
 const setupAndStartServer=() =>{
-    
+    const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended:true}));
 
@@ -18,6 +18,14 @@ const setupAndStartServer=() =>{
         if(process.env.DB_SYNC){
             db.sequelize.sync({alter: true});
         }
+
+        // sendBasicEmail(
+        //     'support@gmail.com',
+        //     'remainder12566@gmail.com',
+        //     'this is testing mail',
+        //     'hello hope u like our service'
+        // );
+        
     })
 }
 setupAndStartServer();
